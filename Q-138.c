1650e9b55fd1f5f63a20b078bf6173ce35aa77c9
Priyanshu45-pixel <jhapriyanshu557@gmail.com>
2025-11-16 14:53:10 +0530

    Add files via upload

---
+++ Q-138.c
@@ -0,0 +1,15 @@
+//Print all enum names and integer values using a loop.
+#include <stdio.h>
+
+int main() {
+    enum Days { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
+    const char *dayNames[] = {
+        "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY",
+        "THURSDAY", "FRIDAY", "SATURDAY"
+    };
+    
+    for (int i = SUNDAY; i <= SATURDAY; i++) {
+        printf("%s = %d\n", dayNames[i], i);
+    }
+    return 0;
+}
